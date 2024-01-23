e0197395b5b8c278a491af8a331a28d90cb96a7f
timothy1202 <timothy1202@naver.com>
2024-01-23 21:08:12 +0900

    Revert "언리얼 프로젝트 추가"
    This reverts commit 1ca71eebf50eced0097f78fe5b5582b7fe32f4a7.

--- Source/GraduationWork2024/GraduationWork2024.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GraduationWork2024.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GraduationWork2024, "GraduationWork2024" );
-
-DEFINE_LOG_CATEGORY(LogGraduationWork2024)
- 
