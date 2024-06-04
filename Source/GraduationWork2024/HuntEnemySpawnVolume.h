f8866b7444eb6664bb09e975785017c4bc822382
sehyeok3406 <105164287+sehyeok3406@users.noreply.github.com>
2024-06-05 08:52:39 +0900

    머지 및 수정 완료

--- Source/GraduationWork2024/HuntEnemySpawnVolume.h
+++ Source/GraduationWork2024/HuntEnemySpawnVolume.h
@@ -45,14 +45,6 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	UFUNCTION()
-	void Volume_OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
-		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-
-	UFUNCTION()
-	void Volume_OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
-		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
-
 	UFUNCTION(BlueprintCallable)
 	void Initialize();
 
